2af827b04668bc9a96fb4f0218b641477746794b
Thiago Lopes <thiagolopes033@hotmail.com>
2023-11-14 14:39:15 -0300

    Update main.js

--- first/public/main.js
+++ first/public/main.js
@@ -5,20 +5,6 @@ if (!gl) {
     throw new Error('WebGL not supported');
 }
 
-// vertexData = [...]
-
-// create buffer
-// load vertexData into buffer
-
-// create vertex shader
-// create fragment shader
-// create program
-// attach shaders to program
-
-// enable vertex attributes
-
-// draw
-
 const vertexData = [
     0, 1, 0,
     1, -1, 0,
@@ -31,18 +17,18 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
-attribute vec3 position;
-void main() {
-    gl_Position = vec4(position, 1);
-}
+    attribute vec3 position;
+    void main() {
+        gl_Position = vec4(position, 1);
+    }
 `);
 gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
-void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
-}
+    void main() {
+        gl_FragColor = vec4(1, 0, 0, 1);
+    }
 `);
 gl.compileShader(fragmentShader);
 
